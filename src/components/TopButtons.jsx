1bdcc9dd41f9e09496f285569333e176248e8858
George Campbell <geocampo010@gmail.com>
2023-07-19 13:58:17 +0100

    for large screens

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -25,7 +25,7 @@ function TopButtons({ setQuery }) {
   ];
 
   return (
-    <div className="flex items-center justify-around my-6 sm:pt-52 ">
+    <div className="flex items-center justify-around my-6 sm:pt-52 xl:pt-0 lg:pt-0 ">
       {cities.map((city) => (
         <button
           key={city.id}
